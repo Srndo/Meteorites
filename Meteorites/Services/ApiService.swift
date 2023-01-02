693b77e1ffa62d8d2f422648c12facaa18ad8abe
Srndo <xsesta06@stud.fit.vutbr.cz>
2023-01-02 23:27:41 +0100

    ApiService request

--- Meteorites/Services/ApiService.swift
+++ Meteorites/Services/ApiService.swift
@@ -5,6 +5,35 @@
 //  Created by Simon Sestak on 02/01/2023.
 //
 
+import Alamofire
 import Foundation
+import PromiseKit
 
-class ApiService {}
+class ApiService {
+    enum Errors: Error {
+        case cannotDecode
+    }
+
+    let decoder = JSONDecoder()
+
+    func request<T: Decodable>(of type: T.Type, urlTask: URLTaskConfiguration) -> Promise<T> {
+        Promise { seal in
+            AF.request(urlTask)
+                .responseData(queue: .global()) { response in
+                    print(response.debugDescription)
+                    switch response.result {
+                    case let .success(data):
+                        // if status code == positive response
+                        if let statusCode = response.response?.statusCode, statusCode < 300, statusCode >= 200 {
+                            guard let decodedData = try? self.decoder.decode(type, from: data) else {
+                                return seal.reject(Errors.cannotDecode)
+                            }
+                            return seal.fulfill(decodedData)
+                        }
+                    case let .failure(error):
+                        return seal.reject(error)
+                    }
+                }
+        }
+    }
+}
