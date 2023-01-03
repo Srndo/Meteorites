89cefd4e5e8503f94ee70c2bd06a1e698452251b
Srndo <xsesta06@stud.fit.vutbr.cz>
2023-01-03 10:33:28 +0100

    Database service base functions

--- Meteorites/Services/DatabaseService.swift
+++ Meteorites/Services/DatabaseService.swift
@@ -5,6 +5,50 @@
 //  Created by Simon Sestak on 02/01/2023.
 //
 
-import Foundation
+import PromiseKit
+import RealmSwift
 
-class DatabaseService {}
+class DatabaseService {
+    enum Errors: Error {
+        case realmNotExists
+    }
+
+    var realm: Realm?
+
+    init() {
+        realm = try? Realm()
+        print(realm?.configuration.fileURL ?? "Realm file doesnt exist")
+    }
+
+    func create<T: Object>(object: T) -> Promise<T> {
+        guard let realm = realm else { return Promise { $0.reject(Errors.realmNotExists) } }
+        return Promise { seal in
+            realm.writeAsync {
+                realm.add(object, update: .modified)
+                seal.fulfill(object)
+            }
+        }
+    }
+
+    func create<T: Object>(objects: [T]) -> Promise<[T]> {
+        guard let realm = realm else { return Promise { $0.reject(Errors.realmNotExists) } }
+        return Promise { seal in
+            realm.writeAsync {
+                objects.forEach { realm.add($0, update: .modified) }
+                seal.fulfill(objects)
+            }
+        }
+    }
+
+    func getElementsFromDB<T: Object>(of _: T.Type) -> [T]? {
+        realm?.objects(T.self).toArray()
+    }
+
+    func getElementsFromDBFiltred<T: Object>(of _: T.Type, by filter: String, args: Any...) -> [T]? {
+        realm?.objects(T.self).filter(filter, args).toArray()
+    }
+}
+
+extension Results {
+    func toArray() -> [Element] { compactMap { $0 } }
+}
