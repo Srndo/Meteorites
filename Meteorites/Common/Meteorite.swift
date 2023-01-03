86d978296a6148140dd60a6d932f25b598e6f52d
Srndo <xsesta06@stud.fit.vutbr.cz>
2023-01-03 10:33:57 +0100

    Realm Object - Meteorite
    With fetching and saving data from server

---
+++ Meteorites/Common/Meteorite.swift
@@ -0,0 +1,63 @@
+//
+//  Meteorite.swift
+//  Meteorites
+//
+//  Created by Simon Sestak on 03/01/2023.
+//
+
+import RealmSwift
+
+final class Meteorite: Object, Decodable {
+    @Persisted(primaryKey: true) var id: String
+    @Persisted var name: String?
+    @Persisted var recclass: String?
+    @Persisted var fall: Fall?
+    @Persisted var year: String?
+    @Persisted var reclat: String?
+    @Persisted var reclong: String?
+    @Persisted var geolocation: Geolocation?
+    var nametype: Nametype?
+    var mass: String?
+    var computedRegionCbhkFwbd, computedRegionNnqa25F4: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name, id, nametype, recclass, mass, fall, year, reclat, reclong, geolocation
+        case computedRegionCbhkFwbd = ":@computed_region_cbhk_fwbd"
+        case computedRegionNnqa25F4 = ":@computed_region_nnqa_25f4"
+    }
+
+    convenience init(from decoder: Decoder) throws {
+        self.init()
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decodeIfPresent(String.self, forKey: .name)
+        recclass = try container.decodeIfPresent(String.self, forKey: .recclass)
+        fall = try container.decodeIfPresent(Fall.self, forKey: .fall)
+        year = try container.decodeIfPresent(String.self, forKey: .year)
+        reclat = try container.decodeIfPresent(String.self, forKey: .reclat)
+        reclong = try container.decodeIfPresent(String.self, forKey: .reclong)
+        geolocation = try container.decodeIfPresent(Geolocation.self, forKey: .geolocation)
+//        geolocation = try decoder.singleValueContainer().decode(Geolocation.self)
+        nametype = try container.decodeIfPresent(Nametype.self, forKey: .nametype)
+//        nametype = try decoder.singleValueContainer().decode(Nametype.self)
+        mass = try container.decodeIfPresent(String.self, forKey: .mass)
+        computedRegionCbhkFwbd = try container.decodeIfPresent(String.self, forKey: .computedRegionCbhkFwbd)
+        computedRegionNnqa25F4 = try container.decodeIfPresent(String.self, forKey: .computedRegionNnqa25F4)
+    }
+}
+
+enum Fall: String, Codable, Equatable, PersistableEnum {
+    case fell = "Fell"
+    case found = "Found"
+}
+
+class Geolocation: EmbeddedObject, Decodable {
+    @Persisted var latitude: String
+    @Persisted var longitude: String
+}
+
+enum Nametype: String, Codable {
+    case valid = "Valid"
+}
+
+typealias Meteorits = [Meteorite]
--- Meteorites/Stories/List/ListViewModel.swift
+++ Meteorites/Stories/List/ListViewModel.swift
@@ -12,8 +12,13 @@ class ListViewModel: BaseViewModel {
         let endPoint = EndPoints.filtered(fromDate: "2011-01-01T00:00:00".getDate(), fall: .fell)
         let urlTaks = URLTask(endPoint: endPoint)
         apiService.request(of: Meteorits.self, urlTask: urlTaks)
-            .done { meteorites in
-                print(meteorites)
+            .done { [weak self] meteorites in
+                self?.databaseService.create(objects: meteorites)
+                    .done { meteorites in
+                        print("Saved \(meteorites.count) meteorites")
+                    }.catch { error in
+                        print(error)
+                    }
             }.catch { error in
                 print(error)
             }
